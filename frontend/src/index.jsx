a71f1ade5b3fcf95b1ce215b164dd830fbc3dac7
emidemir <emirhan.2521demir@gmail.com>
2025-11-27 12:59:20 +0300

    Redux and folder setup complete

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// Google OAuth import
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+// Page imports for router
+import App from './App';
+import ProfilePage from './pages/ProfilePage'
+
+
+// Redux imports
+import { store } from './app/store'
+import {Provider} from 'react-redux'
+// Route imports
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+	{path: '/', element:<App/>},
+	{path:'/profile', element:<ProfilePage/>},
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_OAUTH_CLIENT_ID}>
+				<RouterProvider router={router}/>
+			</GoogleOAuthProvider>
+		</Provider>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
